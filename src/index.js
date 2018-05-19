af6012c0667d395e8295c9a346b2a22d46eced9a
bijitpalHome <bijitpal@gmail.com>
2018-05-20 00:45:09 +0530

    'Components

--- src/index.js
+++ src/index.js
@@ -5,9 +5,37 @@ import ReactDom from 'react-dom'
   //return <h2>{new Date().toLocaleTimeString()}</h2>
 //}
 
-setInterval(function(){
+//one way of writing component
+// function Cartoon(props){
+//  return <h1>Hello, {props.name}</h1>
+// }
+
+//one way of writing component
+// class Cartoon extends React.Component{
+//   render(){
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+
+//can use component inside another component
+function Cartoon(props){
+ return <h1>Hello, {props.name} on {props.show}</h1>
+}
+
+function Show(){
+  return <div>
+           <Cartoon name='Pikachoo' show='Pokemon' />
+           <Cartoon name='Jasmine' show='Alladin' />
+        </div>
+}
+
 ReactDom.render(
- <h2>Time Now: {new Date().toLocaleTimeString()}</h2>,
+ //<h2>Time Now: {new Date().toLocaleTimeString()}</h2>,
+ //<Cartoon name='Bijit'/>,
+ <Show />,
   document.getElementById('root')
 );
-},1000);
+
+//Always a component return single html element.That means, a single <div> can
+//contain multiple elements inside it. But, on parent level, it has to be
+//a single element, such as the <div>
